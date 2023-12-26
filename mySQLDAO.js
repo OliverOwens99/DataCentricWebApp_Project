6298986f2ae6d8668c99e86e1dc897c6bcd7dd68
olly <g00409776@atu.ie>
2023-12-26 15:36:10 +0000

    intial commit

---
+++ mySQLDAO.js
@@ -0,0 +1,36 @@
+const e = require('express');
+var pmysql = require('promise-mysql');
+var pool;
+
+
+pmysql.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'proj2023'
+})
+    .then(p => {
+        pool = p
+    })
+    .catch(e => {
+        console.log("pool error:" + e)
+    })
+
+ function getstores() {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM store')
+        .then(result => {
+            resolve(result)
+        })
+        .catch(e => {
+            reject(e)
+        })
+    })
+
+    
+
+    
+    
+}
+module.exports = {getstores};
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+var app = express()
+var port = 3000
+var mySQLDAO = require('./mySQLDAO.js')
+
+let ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    //display a file called home.ejs
+    
+    res.render('home');
+})
+
+
+app.get('/stores', (req, res) => {
+    mySQLDAO.getstores()
+    .then(result => {
+        console.log(result)
+        res.render('stores', {stores: result}) // stores is the name of the variable in stores.ejs
+    })
+    .catch(e => {
+        res.send(e)
+    })
+})
+
+app.post('/stores', (req, res) => {
+    mySQLDAO.getstores()
+    .then(result => {
+        console.log(result)
+        res.render('stores', {stores: result}) // stores is the name of the variable in stores.ejs
+    })
+    .catch(e => {
+        res.send(e)
+    })
+})
+
+app.get('/stores/edit/:sid', (req, res) => {
+    
+})
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
